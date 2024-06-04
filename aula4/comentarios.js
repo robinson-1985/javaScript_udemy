265e1a9aa29b919047a451f47e84482a1d21c527
robinson <robinpaulista85@gmail.com>
2024-06-04 18:45:29 -0300

    Comentários em JS

---
+++ aula4/comentarios.js
@@ -0,0 +1,8 @@
+// E escrever um comentário de uma linha
+console.log('Hello World!'); // Escreve "Hello World!" no console
+console.log(12, 15.89, 'Hello World!'); // Escreve 12, 15.89 e "Hello World!" no console
+/*
+  Este é um comentário de bloco.
+  Ele pode ser usado para descrever o que o código faz.
+  Ou para desativar uma parte do código.
+*/
