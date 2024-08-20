85f44b54f74c345b7acaef863267c71214de26b8
robinson <robinpaulista85@gmail.com>
2024-08-20 13:28:59 -0300

    tipos de dados primitivos em JS

---
+++ aula10/index19.js
@@ -0,0 +1,16 @@
+// Tipos de Dados primitivos
+
+const nome = 'João'; //string
+const nome1 = "João"; //string
+const nome2 = `João`; //string
+
+const num1 = 10; //number
+const num2 = 10.52; //number
+
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+
+const aprovado = true; // Boolean = true, false (lógico)
+const reprovado = false; // Boolean = true, false (lógico)
+
+console.log(typeof nome, nome);
