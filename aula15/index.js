7290c95a3573ce4601de425186caf2271f0bec91
robinson <robinpaulista85@gmail.com>
2024-08-28 22:55:27 -0300

    objeto `Math` em `JavaScript`

---
+++ aula15/index.js
@@ -0,0 +1,28 @@
+// Método Math.round() arredonda o número para o inteiro mais próximo
+
+let num1 = 9;
+console.log(num1**0.5); // raiz quadrada
+
+
+// let num2 = Math.floor(num1); // arredonda para baixo
+
+// let num2 = Math.ceil(num1); // arredonda para cima
+
+// let num2 = Math.round(num1); // arredonda para o inteiro mais próximo
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // retorna o maior número
+
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // retorna o menor número
+
+// console.log(Math.random()); // retorna um número aleatório entre 0 e 1
+
+// const aleatorio = Math.random() * (10 - 5) + 5; // retorna um número aleatório entre 5 e 10
+
+//console.log(aleatorio);
+
+// console.log(Math.PI); // retorna o valor de PI
+
+// console.log(Math.pow(2, 10)); // retorna a potência de 2 elevado a 10
+// console.log(2**10); // retorna a potência de 2 elevado a 10 de forma mais simples
+
+// console.log(100 / 0); // retorna Infinity (única exceção que não gera erro)
