a12201669a8c29490a9396f6dc5300beed265034
robinson <robinpaulista85@gmail.com>
2025-06-24 20:57:32 -0300

    feat: aprendizado sobre o método map() em JavaScript

---
+++ aula66/index.js
@@ -0,0 +1,16 @@
+// Map: é uma função que retorna valores com base em um array
+
+// Dobre os números
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numerosEmDobro = numeros.map(function(valor){
+  return `Robbie: ${valor * 2}`;
+});
+
+
+
+
+
+
+
+
